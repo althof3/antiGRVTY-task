2aef2d5d0199784567bcee6fe60d306ebb521955
althof <althoframdhan3@gmail.com>
2020-09-08 13:39:39 +0700

    add assets and layout starter kit

---
+++ src/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Layout() {
+  return (
+    <div>
+      {/* Home Section */}
+      {/* Tipe & Warna Section */}
+      {/* Eksterior Section */}
+      {/* Interior Section */}
+      {/* Spesifikasi Section */}
+      {/* Daftar Harga Section */}
+    </div>
+  )
+}
+
+export default Layout
